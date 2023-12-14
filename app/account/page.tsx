d2bde8af6778b6a90568cc41cc4d3eff206f0960
kotaro1574 <shake1574@gmail.com>
2023-12-13 18:15:19 -0800

    create: AccountPage

---
+++ app/account/page.tsx
@@ -0,0 +1,16 @@
+import { cookies } from "next/headers"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+
+import { Database } from "@/types/supabase"
+
+import AccountForm from "./account-form"
+
+export default async function AccountPage() {
+  const supabase = createServerComponentClient<Database>({ cookies })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  return <AccountForm session={session} />
+}
