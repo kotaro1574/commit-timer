e41cce6c677150ab4e2ea7153903e48d02cbacc1
kotaro1574 <shake1574@gmail.com>
2023-12-15 18:35:13 -0800

    update: commitsを取得＆表示

--- app/commits/page.tsx
+++ app/commits/page.tsx
@@ -1,37 +1,52 @@
+import { cookies } from "next/headers"
 import Link from "next/link"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { Database } from "@/types/supabase"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
 
-export default function CommitsPage() {
+export default async function CommitsPage() {
+  const supabase = createServerComponentClient<Database>({ cookies })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (!session) return null
+
+  const user = session.user
+
+  const { data, error, status } = await supabase
+    .from("commits")
+    .select(`title, time`)
+    .eq("user_id", user.id)
+
+  if (!data) return null
+
+  if (error && status !== 406) {
+    throw error
+  }
+
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Commits
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
+      <div className="flex max-w-[980px] flex-col items-start gap-4">
+        <div className="flex w-full justify-between">
+          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+            Commits
+          </h1>
+          <Link href={"/"} className={buttonVariants({ variant: "ghost" })}>
+            <Icons.plus className="mr-2 h-4 w-4" />
+            new commit
+          </Link>
+        </div>
+        <div className="flex gap-4">
+          {data.map((commit) => (
+            <div className="flex flex-col items-start gap-2">
+              <Button>{commit.title}</Button>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
