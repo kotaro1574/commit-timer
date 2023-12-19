501a87fcfe14a26041c5424ba51d53d4730a2d9f
kotaro1574 <shake1574@gmail.com>
2023-12-19 10:08:21 -0800

    add: textarea

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -6,21 +6,27 @@ import Timer from "./timer"
 
 export default function TimerPage() {
   const searchParams = useSearchParams()
-  const title = searchParams.get("title") || ""
-  const duration = Number(searchParams.get("time")) || 0
+  const title = searchParams.get("title")
+  const duration = Number(searchParams.get("time"))
 
   return (
     <section className="grid gap-6">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Timer
-          {title && (
-            <span className="mr-2 text-lg text-gray-500">{`（${title}）`}</span>
-          )}
-        </h1>
-        <div className="mx-auto">
-          <Timer duration={duration} />
-        </div>
+        {!title && !duration ? (
+          <div className="mx-auto">dddds</div>
+        ) : (
+          <>
+            <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+              Timer
+              {title && (
+                <span className="mr-2 text-lg text-gray-500">{`（${title}）`}</span>
+              )}
+            </h1>
+            <div className="mx-auto">
+              <Timer duration={duration} />
+            </div>
+          </>
+        )}
       </div>
     </section>
   )
