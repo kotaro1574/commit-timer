d4c45dfb0f7d4a6650474f19c689b7a38f6790f8
kotaro1574 <shake1574@gmail.com>
2023-12-19 14:22:12 -0800

    update: Timer

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -1,32 +1,56 @@
 "use client"
 
+import { useState } from "react"
 import { useSearchParams } from "next/navigation"
+import { FormProvider } from "react-hook-form"
+import { z } from "zod"
 
 import Timer from "./timer"
+import TimerForm from "./timer-form"
+import { useCreateResultForm } from "./useCreateResult"
+
+const formSchema = z.object({
+  title: z.string(),
+  time: z.coerce.number(),
+  description: z.string(),
+})
 
 export default function TimerPage() {
+  const [isStart, setIsStart] = useState(false)
   const searchParams = useSearchParams()
   const title = searchParams.get("title")
-  const duration = Number(searchParams.get("time"))
+  const time = Number(searchParams.get("time"))
+  const form = useCreateResultForm({ title, time })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {}
 
   return (
     <section className="grid gap-6">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        {!title && !duration ? (
-          <div className="mx-auto">dddds</div>
-        ) : (
-          <>
-            <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-              Timer
-              {title && (
-                <span className="mr-2 text-lg text-gray-500">{`（${title}）`}</span>
-              )}
-            </h1>
-            <div className="mx-auto">
-              <Timer duration={duration} />
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Timer
+          {title && (
+            <span className="mr-2 text-lg text-gray-500">{`（${title}）`}</span>
+          )}
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          {`Jot down what you're committing to and kick off the timer!`}
+        </p>
+        <FormProvider {...form}>
+          {!isStart ? (
+            <div className="w-full">
+              <TimerForm
+                onStart={() => {
+                  setIsStart(true)
+                }}
+              />
             </div>
-          </>
-        )}
+          ) : (
+            <div className="mx-auto">
+              <Timer duration={time} />
+            </div>
+          )}
+        </FormProvider>
       </div>
     </section>
   )
---
+++ app/timer/useCreateResult.ts
@@ -0,0 +1,31 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm, useFormContext } from "react-hook-form"
+import { z } from "zod"
+
+const formSchema = z.object({
+  title: z.string(),
+  time: z.coerce.number(),
+  description: z.string(),
+})
+
+type CreateResultInput = z.infer<typeof formSchema>
+
+type Params = {
+  title: string | null
+  time: number | null
+}
+
+export const useCreateResultForm = ({ title, time }: Params) => {
+  return useForm<CreateResultInput>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: title ?? "",
+      time: time ?? 0,
+      description: "",
+    },
+  })
+}
+
+export const useCreateResultFormContext = () => {
+  return useFormContext<CreateResultInput>()
+}
