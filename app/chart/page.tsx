2540fedc057006d4650df76e5411bed67c50d142
kotaro1574 <shake1574@gmail.com>
2023-12-24 13:50:19 -0800

    create: AreaChart

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,7 +1,51 @@
-import Link from "next/link"
+"use client"
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import {
+  Area,
+  AreaChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts"
+
+const data = [
+  {
+    year: "2016",
+    Iphone: 4000,
+    Samsung: 2400,
+  },
+  {
+    year: "2017",
+    Iphone: 3000,
+    Samsung: 1398,
+  },
+  {
+    year: "2018",
+    Iphone: 2000,
+    Samsung: 9800,
+  },
+  {
+    year: "2019",
+    Iphone: 2780,
+    Samsung: 3908,
+  },
+  {
+    year: "2020",
+    Iphone: 1890,
+    Samsung: 4800,
+  },
+  {
+    year: "2021",
+    Iphone: 2390,
+    Samsung: 3800,
+  },
+  {
+    year: "2022",
+    Iphone: 3490,
+    Samsung: 4300,
+  },
+]
 
 export default function ChartPage() {
   return (
@@ -10,10 +54,44 @@ export default function ChartPage() {
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Chart
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
+      </div>
+      <div className="h-[400px]">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={730}
+            height={250}
+            data={data}
+            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="year" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="Iphone"
+              stroke="#8884d8"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+            <Area
+              type="monotone"
+              dataKey="Samsung"
+              stroke="#82ca9d"
+              fillOpacity={1}
+              fill="url(#colorPv)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
       </div>
     </section>
   )
