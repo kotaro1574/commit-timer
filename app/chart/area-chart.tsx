217137c0db8d5ada4250893a55dbc267b9703f4c
kotaro1574 <shake1574@gmail.com>
2023-12-28 11:55:43 -0800

    update: area-chartにdata表示

--- app/chart/area-chart.tsx
+++ app/chart/area-chart.tsx
@@ -11,16 +11,19 @@ import {
 
 import { Card } from "@/components/ui/card"
 
-const data = [
-  { week: "Mon", Iphone: 4000, Samsung: 2400 },
-  { week: "Tue", Iphone: 3000, Samsung: 1398 },
-  { week: "Wed", Iphone: 2000, Samsung: 9800 },
-  { week: "Thu", Iphone: 2780, Samsung: 3908 },
-  { week: "Fri", Iphone: 1890, Samsung: 4800 },
-  { week: "Sat", Iphone: 2390, Samsung: 3800 },
-  { week: "Sun", Iphone: 3490, Samsung: 4300 },
-]
-export default function AreaChart() {
+type Props = {
+  data: {
+    day: string
+    [key: string]: number | string
+  }[]
+  colors: {
+    id: string
+    name: string
+    color: string
+  }[]
+}
+
+export default function AreaChart({ data, colors }: Props) {
   return (
     <Card className="h-[400px] w-full p-8">
       <ResponsiveContainer width="100%" height="100%">
@@ -31,32 +34,33 @@ export default function AreaChart() {
           margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
         >
           <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-            </linearGradient>
+            {colors.map((color) => (
+              <linearGradient
+                key={color.id}
+                id={color.id}
+                x1="0"
+                y1="0"
+                x2="0"
+                y2="1"
+              >
+                <stop offset="5%" stopColor={color.color} stopOpacity={0.8} />
+                <stop offset="95%" stopColor={color.color} stopOpacity={0} />
+              </linearGradient>
+            ))}
           </defs>
-          <XAxis dataKey="week" />
+          <XAxis dataKey="day" />
           <YAxis />
           <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="Iphone"
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#colorUv)"
-          />
-          <Area
-            type="monotone"
-            dataKey="Samsung"
-            stroke="#82ca9d"
-            fillOpacity={1}
-            fill="url(#colorPv)"
-          />
+          {colors.map((color) => (
+            <Area
+              key={color.id}
+              type="monotone"
+              dataKey={color.name}
+              stroke={color.color}
+              fillOpacity={1}
+              fill={`url(#${color.id})`}
+            />
+          ))}
         </AreaChartRecharts>
       </ResponsiveContainer>
     </Card>
--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,3 +1,9 @@
+import { cookies } from "next/headers"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+
+import { Database } from "@/types/supabase"
+import { formatDate } from "@/lib/formatDate"
+
 import AreaChart from "./area-chart"
 import BarChart from "./bar-chart"
 import LineChart from "./line-chart"
@@ -5,15 +11,58 @@ import PieChart from "./pie-chart"
 import RadarChart from "./radar-chart"
 
 export default async function ChartPage() {
-  // const supabase = createServerComponentClient<Database>({ cookies })
+  const supabase = createServerComponentClient<Database>({ cookies })
 
-  // const { data, error, status } = await supabase.from("results").select("*")
+  const today = new Date()
+  const lastWeek = new Date(today)
+  lastWeek.setDate(today.getDate() - 6)
 
-  // if (!data) return null
+  const lastWeekDate = lastWeek.toISOString()
 
-  // if (error && status !== 406) {
-  //   throw error
-  // }
+  const { data: commits } = await supabase
+    .from("commits")
+    .select("title, created_at, color")
+  if (!commits) return null
+
+  const { data: committedResults } = await supabase
+    .from("committed-results")
+    .select("*")
+    .gte("created_at", lastWeekDate)
+
+  if (!committedResults) return null
+
+  const getLastWeekDates = (): {
+    day: string
+  }[] => {
+    return Array.from({ length: 7 }, (_, index) => {
+      const date = new Date()
+      date.setDate(today.getDate() - (6 - index))
+      return { day: formatDate(date, "MM/dd") }
+    })
+  }
+
+  const areaChartData = commits.reduce((acc, cur) => {
+    return acc.map((data) => ({
+      ...data,
+      [cur.title]: committedResults.reduce((_acc, _cur) => {
+        if (
+          formatDate(new Date(_cur.created_at), "MM/dd") === data.day &&
+          _cur.title === cur.title
+        ) {
+          return _acc + _cur.time
+        }
+        return _acc
+      }, 0),
+    }))
+  }, getLastWeekDates())
+
+  const areaChartColors = commits.map((commit, index) => {
+    return {
+      id: `color-${index}`,
+      name: commit.title,
+      color: commit.color,
+    }
+  })
 
   return (
     <section className="grid gap-6">
@@ -22,7 +71,7 @@ export default async function ChartPage() {
           Chart
         </h1>
       </div>
-      <AreaChart />
+      <AreaChart data={areaChartData} colors={areaChartColors} />
       <div className="flex gap-6">
         <PieChart />
         <RadarChart />
