7f424da914a79c676f24dcb25b838d98dcdfc889
kotaro1574 <shake1574@gmail.com>
2023-12-29 13:18:22 -0800

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,19 +32,19 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
-export default async function RootLayout({ children }: RootLayoutProps) {
-  const supabase = createServerComponentClient<Database>({ cookies })
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
+export default function RootLayout({ children }: RootLayoutProps) {
+  // const supabase = createServerComponentClient<Database>({ cookies })
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession()
 
-  const user = session?.user
+  // const user = session?.user
 
-  const { data } = await supabase
-    .from("profiles")
-    .select("avatar_url")
-    .eq("id", user?.id ?? "")
-    .single()
+  // const { data } = await supabase
+  //   .from("profiles")
+  //   .select("avatar_url")
+  //   .eq("id", user?.id ?? "")
+  //   .single()
 
   return (
     <>
@@ -58,10 +58,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
-              <SiteHeader
-                session={session}
-                avatar_url={data?.avatar_url ?? ""}
-              />
+              <SiteHeader session={null} avatar_url={""} />
               <div className="container max-w-[980px] flex-1 pb-8 pt-6 md:py-10">
                 {children}
               </div>
