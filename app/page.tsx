0c4c37a1ee01e6ea8c3c6cc1adeb0f622709e8ff
kotaro1574 <shake1574@gmail.com>
2024-01-02 14:29:21 -0800

    update: topPage調整

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,15 @@ import AuthForm from "@/components/form/auth-form"
 
 export default function HomePage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Commit
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
+    <section className="grid gap-6">
+      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+        Login
+      </h1>
+      <p className="max-w-[700px] text-lg text-muted-foreground">
+        Enter your email address and we will send you a magic link to log in
+        without having to remember your password.
+      </p>
 
-      <div className="col-6">
-        <h1 className="header">Supabase Auth + Storage</h1>
-        <p className="">
-          Experience our Auth and Storage through a simple profile management
-          example. Create a user profile and upload an avatar image. Fast,
-          simple, secure.∏
-        </p>
-      </div>
       <div className="col-6 auth-widget">
         <AuthForm />
       </div>
